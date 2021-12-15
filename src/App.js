c35898663f048241858590fda51b2a8f509dec36
Afhaam k <afhaamcoc@gmail.com>
2021-12-15 16:21:37 +0530

    url changed locally

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import  store from './redux/store';
 import { Provider } from 'react-redux';
 import axios from 'axios'
 
-axios.defaults.baseURL='http://localhost:5000'
+axios.defaults.baseURL='http://server.afhaam.tech'
  axios.defaults.withCredentials=true
 
 
